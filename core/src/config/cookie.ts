7991987ac92bb627105a4964ab51ddb1a8add757
Faizal Raheem <5zziiihhh@gmail.com>
2026-07-08 22:02:27 +0530

    fix: update cookie configurations for production environment and clean up tsconfig settings

--- core/src/config/cookie.ts
+++ core/src/config/cookie.ts
@@ -1,16 +1,19 @@
 import { CookieOptions } from "express";
-import { EnvConfig } from "./env.js";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const accesCookieConfig: CookieOptions = {
   maxAge: 15 * 60 * 1000,
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: isProduction,
+  sameSite: "lax",
+  path: "/",
 };
 
 export const refreshCookieConfig: CookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: isProduction,
+  sameSite: "lax",
+  path: "/",
 };
